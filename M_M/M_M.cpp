ba23a30c137ba88385ee98f45e8955a246369c23
up2300536 <up2300536@myport.ac.uk>
2026-03-15 23:50:08 +0000

    Delete M_M directory

--- M_M/M_M.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "M_M.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M_M, "M_M" );
-
-DEFINE_LOG_CATEGORY(LogM_M)
