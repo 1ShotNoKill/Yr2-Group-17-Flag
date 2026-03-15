99e513c9b5a8158a9898d460da095a2ca3597fb7
up2300536 <up2300536@myport.ac.uk>
2026-03-15 23:59:40 +0000

    Updated Player and Enemy Movement, Pistol Weapon, and Roaming Phase

---
+++ Source/M_M/M_M.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class M_M : ModuleRules
+{
+	public M_M(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"M_M",
+			"M_M/Variant_Horror",
+			"M_M/Variant_Horror/UI",
+			"M_M/Variant_Shooter",
+			"M_M/Variant_Shooter/AI",
+			"M_M/Variant_Shooter/UI",
+			"M_M/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
